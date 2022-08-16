12a86ec8f2805fcbf80df2716eb5dcbbc1e9c2c5
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-08-16 19:51:37 +0100

    controllers set up

--- src/main/java/com/example/FoldersHW/controllers/FileController.java
+++ src/main/java/com/example/FoldersHW/controllers/FileController.java
@@ -1,4 +1,33 @@
 package com.example.FoldersHW.controllers;
 
+import com.example.FoldersHW.models.File;
+import com.example.FoldersHW.repositories.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class FileController {
+    @Autowired
+    FileRepository fileRepository;
+
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<File>> getAllFiles() {
+        return new ResponseEntity<>(fileRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/files/{id}")
+    public ResponseEntity<Optional<File>> getFile(@PathVariable Long id) {
+        return new ResponseEntity<>(fileRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/files")
+    public ResponseEntity<File> createFile(@RequestBody File file) {
+        fileRepository.save(file);
+        return new ResponseEntity<>(file, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/example/FoldersHW/controllers/FolderController.java
+++ src/main/java/com/example/FoldersHW/controllers/FolderController.java
@@ -1,4 +1,33 @@
 package com.example.FoldersHW.controllers;
 
+import com.example.FoldersHW.models.Folder;
+import com.example.FoldersHW.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class FolderController {
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders() {
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "folders/{id}")
+    public ResponseEntity<Optional<Folder>> getFolder(@PathVariable Long id) {
+        return new ResponseEntity<>(folderRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/folders")
+    public ResponseEntity<Folder> createFolder(@RequestBody Folder folder) {
+        folderRepository.save(folder);
+        return new ResponseEntity<>(folder, HttpStatus.CREATED);
+    }
 }
