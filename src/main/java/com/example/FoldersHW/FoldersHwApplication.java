f9176a23b819d6cbeb16b5d887e2bd8c2c467adb
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-08-16 17:52:37 +0100

    inital commit

---
+++ src/main/java/com/example/FoldersHW/FoldersHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoldersHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoldersHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoldersHwApplication.class, args);
+	}
+
+}
